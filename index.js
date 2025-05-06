ceab77b5fd67146720f063a2b1220a88816953ba
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2025-05-05 21:25:25 -0400

    cycling technically works on Enter keydown. Things still feel messy. Need to think

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let wordleContainer = document.querySelector(".wordle-container");
 
+let guessCounter = 0;
+
 function isLetter(letter) {
   return (
     /^[a-zA-Z]$/.test(letter) || letter === "Backspace" || letter == "Delete"
@@ -23,13 +25,46 @@ for (let i = 0; i < 6; i++) {
   }
 }
 
-for (let i = 1; i < 7; i++) {
-  let guessRowCheck = document.querySelector(`.row-${i}`);
-  guessRowCheck.addEventListener("change", (e) => {
-    let currentUserGuess = "";
-    for (const child of guessRowCheck.children) {
-      currentUserGuess += child.value;
+let justTrying = () => {
+  for (let i = 1; i < 7; i++) {
+    let guessRowCheck = document.querySelector(`.row-${i}`);
+    guessRowCheck.addEventListener("change", (e) => {
+      let currentUserGuess = "";
+      for (const child of guessRowCheck.children) {
+        currentUserGuess += child.value;
+      }
+    });
+  }
+};
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    if (guessCounter === 5) {
+      let pTag = document.createElement("p");
+      pTag.innerText = "All Done";
+      wordleContainer.append(pTag);
     }
-    console.log(`row${i}:${currentUserGuess}`);
-  });
+    guessCounter++;
+    console.log("test");
+    activeRow();
+  }
+});
+
+function activeRow() {
+  for (let i = 0; i < wordleContainer.childElementCount; i++) {
+    const row = wordleContainer.children[i];
+    const inputs = row.querySelectorAll("input");
+
+    if (i === guessCounter) {
+      inputs.forEach((input) => {
+        input.removeAttribute("disabled");
+      });
+    } else {
+      inputs.forEach((input) => {
+        input.setAttribute("disabled", true);
+      });
+    }
+  }
 }
+
+activeRow();
