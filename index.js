d40f23175406a03f61dd74506435d44072d259f5
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2025-05-06 12:40:22 -0400

    functionality for if a letter is correct and on submission changes color is working. Need logic for if a letter is included but in the wrong spot.

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 let wordleContainer = document.querySelector(".wordle-container");
-
 let guessCounter = 0;
+let wordOfTheDay = "stoop";
 
+// Only allowing certain keys to interact with the page
 function isLetter(letter) {
   return (
-    /^[a-zA-Z]$/.test(letter) || letter === "Backspace" || letter == "Delete"
+    /^[a-zA-Z]$/.test(letter) ||
+    letter === "Backspace" ||
+    letter === "Delete" ||
+    letter === "Tab"
   );
 }
-
+// Creating the rows and inputs for each row
 for (let i = 0; i < 6; i++) {
   let guessingRow = document.createElement("ul");
   guessingRow.className = `row-${i + 1}`;
@@ -19,36 +23,58 @@ for (let i = 0; i < 6; i++) {
         e.preventDefault();
       }
     });
+    if (z === 0) {
+      letterInputSquare.setAttribute("autofocus", true);
+    }
     letterInputSquare.setAttribute("maxlength", "1");
     letterInputSquare.setAttribute("id", `row-${i + 1}-input${z + 1}`);
     guessingRow.append(letterInputSquare);
   }
 }
-
-let justTrying = () => {
-  for (let i = 1; i < 7; i++) {
-    let guessRowCheck = document.querySelector(`.row-${i}`);
-    guessRowCheck.addEventListener("change", (e) => {
-      let currentUserGuess = "";
-      for (const child of guessRowCheck.children) {
-        currentUserGuess += child.value;
-      }
-    });
-  }
-};
+// Checks the active row for the users guess and validates all 5 inputs have letters
+function userGuessLengthAndValue() {
+  const row = wordleContainer.children[guessCounter];
+  const inputs = row.querySelectorAll("input");
+  let rowInputObject = {
+    inputLength: 0,
+    inputValue: "",
+  };
+  inputs.forEach((input) => {
+    if (input.value.length === 1) {
+      rowInputObject.inputLength++;
+      rowInputObject.inputValue += input.value;
+    }
+  });
+  return rowInputObject;
+}
 
 document.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
+  let rowInfo = userGuessLengthAndValue();
+  if (e.key === "Enter" && rowInfo.inputLength === 5) {
     if (guessCounter === 5) {
       let pTag = document.createElement("p");
       pTag.innerText = "All Done";
       wordleContainer.append(pTag);
     }
     guessCounter++;
-    console.log("test");
+    submittedGuessCheck(rowInfo.inputValue);
     activeRow();
   }
 });
+
+function submittedGuessCheck(userGuess) {
+  for (let i = 0; i < 5; i++) {
+    if (userGuess[i] === wordOfTheDay[i]) {
+      let specificInput = document.querySelector(
+        `#row-${guessCounter}-input${i + 1}`
+      );
+      specificInput.style.backgroundColor = "lightgreen";
+      console.log(`letter at position ${i} is correct`);
+    } else if (wordOfTheDay.includes(userGuess[i])) {
+      console.log(`${userGuess[i]} is included but wrong spot`);
+    }
+  }
+}
 
 function activeRow() {
   for (let i = 0; i < wordleContainer.childElementCount; i++) {
